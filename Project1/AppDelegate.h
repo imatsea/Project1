044376c21d704f2e2f76d09cf909d20611291ae9
Iris Qiu <IrisQiu@user3-23-75.wireless.utoronto.ca>
2015-05-14 13:17:14 -0400

    Initial Commit

---
+++ Project1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project1
+//
+//  Created by Iris Qiu on 15/5/14.
+//  Copyright (c) 2015年 Iris Qiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Project1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project1
+//
+//  Created by Iris Qiu on 15/5/14.
+//  Copyright (c) 2015年 Iris Qiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
